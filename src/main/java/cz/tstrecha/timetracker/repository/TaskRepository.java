ae98bdae0002284ec9610cb88cba3faa178f1aa3
JBarcuk <kubabarcuk@gmail.com>
2023-03-09 21:29:13 +0100

    #24 - Search in tasks

--- src/main/java/cz/tstrecha/timetracker/repository/TaskRepository.java
+++ src/main/java/cz/tstrecha/timetracker/repository/TaskRepository.java
@@ -10,6 +10,6 @@ public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
 
     @Query(value = "SELECT * FROM task WHERE (name_simple ILIKE :query OR custom_id ILIKE :query" +
             " OR CONCAT(custom_id, CONCAT(' - ', name_simple)) ILIKE :query ) " +
-            "AND user_id = :loggedUserId LIMIT :limitFromQuery", nativeQuery = true)
-    List<TaskEntity> searchForTasks(String query, Long loggedUserId, Long limitFromQuery);
+            "AND user_id = :loggedUserId LIMIT :resultLimit", nativeQuery = true)
+    List<TaskEntity> searchForTasks(String query, Long loggedUserId, Long resultLimit);
 }
