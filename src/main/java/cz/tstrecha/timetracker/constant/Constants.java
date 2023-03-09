410539ab5fd4a2deee3e09f12acb7faf8ef62924
JBarcuk <127048886+JBarcuk@users.noreply.github.com>
2023-03-09 19:57:36 +0100

    Apply suggestions from code review
    Co-authored-by: Tomáš Střecha <87204346+TStrecha@users.noreply.github.com>

--- src/main/java/cz/tstrecha/timetracker/constant/Constants.java
+++ src/main/java/cz/tstrecha/timetracker/constant/Constants.java
@@ -3,5 +3,5 @@ package cz.tstrecha.timetracker.constant;
 public class Constants {
     public static final String V1_CONTROLLER_ROOT = "/api/v1/";
 
-    public static final int MIN_SEARCH_LENGTH = 3;
+    public static final Integer MIN_SEARCH_LENGTH = 3;
 }
