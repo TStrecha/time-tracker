e34d9f270dc25f39ebb834ab632e652bacf974af
Tomáš Střecha <tomas.strecha@beit.io>
2023-03-10 16:50:52 +0100

    #25 - WIP: Test enhancements

--- src/test/java/cz/tstrecha/timetracker/IntegrationTest.java
+++ src/test/java/cz/tstrecha/timetracker/IntegrationTest.java
@@ -8,20 +8,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.support.TransactionTemplate;
-
-import java.util.Arrays;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
 public abstract class IntegrationTest {
-
-    private static final String TEST_PROFILE = "testprod";
-    private static final String TEST_LOCAL_PROFILE = "testlocal";
 
     @Autowired
     protected MockMvc mvc;
@@ -39,15 +35,9 @@ public abstract class IntegrationTest {
     @Autowired
     protected UserRepository userRepository;
 
-    @Autowired
-    private ConfigurableEnvironment env;
-
     @BeforeEach
     public void init(){
         cleanUpDB();
-        if(!Arrays.asList(env.getActiveProfiles()).contains(TEST_PROFILE)){
-            env.setActiveProfiles(TEST_LOCAL_PROFILE);
-        }
     }
 
     protected void cleanUpDB() {
