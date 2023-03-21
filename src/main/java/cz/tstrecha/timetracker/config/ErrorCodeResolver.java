5040ecd074978fd5515a3fd5d7fc02eb96ef2293
JBarcuk <kubabarcuk@gmail.com>
2023-03-21 10:37:09 +0100

    #39 - Localization enhancements

--- src/main/java/cz/tstrecha/timetracker/config/ErrorCodeResolver.java
+++ src/main/java/cz/tstrecha/timetracker/config/ErrorCodeResolver.java
@@ -1,6 +1,7 @@
 package cz.tstrecha.timetracker.config;
 
 import cz.tstrecha.timetracker.constant.ErrorTypeCode;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
@@ -9,11 +10,14 @@ import java.util.ResourceBundle;
 @Component
 public class ErrorCodeResolver {
 
-    private final Locale defaultLocale = new Locale.Builder().setLanguage("cs").setRegion("CZ").build();
-
-    private final ResourceBundle errorCodeBundle = ResourceBundle.getBundle("error_codes", defaultLocale);
+    private ResourceBundle errorCodeBundle;
 
     public String resolveException(ErrorTypeCode errorTypeCode){
+        Locale locale = LocaleContextHolder.getLocale();
+        if (ClassLoader.getSystemResource("error_codes_" + locale + ".properties") == null){
+            locale = new Locale.Builder().setLanguage("cs").setRegion("CZ").build();
+        }
+        errorCodeBundle = ResourceBundle.getBundle("error_codes", locale);
         return errorCodeBundle.getString(errorTypeCode.getLocalizationCode());
     }
 }
