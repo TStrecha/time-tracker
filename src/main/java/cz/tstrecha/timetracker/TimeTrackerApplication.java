548c4fb400e74798ad8bbe441f7c2b8b8cf92436
Tomáš Střecha <tomas.strecha@beit.io>
2023-03-21 23:04:40 +0100

    #42 - Test and object mapper bean cleanup

--- src/main/java/cz/tstrecha/timetracker/TimeTrackerApplication.java
+++ src/main/java/cz/tstrecha/timetracker/TimeTrackerApplication.java
@@ -1,22 +1,12 @@
 package cz.tstrecha.timetracker;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TimeTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TimeTrackerApplication.class, args);
-    }
-
-    @Bean
-    public ObjectMapper objectMapper(){
-        var mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        return mapper;
     }
 }
