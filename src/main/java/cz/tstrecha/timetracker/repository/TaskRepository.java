aad8c02427ad394a48bc008e1ebfe98c2afc06b2
JBarcuk <kubabarcuk@gmail.com>
2023-03-26 15:39:41 +0200

    #40 - Applied suggestions from review

--- src/main/java/cz/tstrecha/timetracker/repository/TaskRepository.java
+++ src/main/java/cz/tstrecha/timetracker/repository/TaskRepository.java
@@ -8,7 +8,9 @@ import cz.tstrecha.timetracker.repository.entity.TaskEntity;
 import cz.tstrecha.timetracker.repository.entity.TaskEntity_;
 import cz.tstrecha.timetracker.repository.entity.UserEntity;
 import cz.tstrecha.timetracker.util.FilterUtils;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,6 +19,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<TaskEntity, Long>, JpaSpecificationExecutor<TaskEntity> {
@@ -35,24 +38,30 @@ public interface TaskRepository extends JpaRepository<TaskEntity, Long>, JpaSpec
             List<Predicate> predicates = new ArrayList<>();
             predicates.add(cb.equal(root.get(TaskEntity_.USER), loggedUser.getUserEntity()));
 
-            for (TaskField taskField : taskFilter.getFieldFilters().keySet()) {
-                switch (taskField) {
-                    case ID -> predicates.add(cb.equal(root.get(TaskEntity_.ID), taskFilter.getFieldFilters().get(taskField)));
-                    case NAME_SIMPLE -> predicates.add(cb.like(cb.lower(root.get(TaskEntity_.NAME_SIMPLE)), FilterUtils.enrichLikeStatements(taskFilter.getFieldFilters().get(taskField))));
-                    case CUSTOM_ID -> predicates.add(cb.like(root.get(TaskEntity_.CUSTOM_ID).as(String.class), FilterUtils.enrichLikeStatements(taskFilter.getFieldFilters().get(taskField))));
-                    case NOTE -> predicates.add(cb.like(cb.lower(root.get(TaskEntity_.NOTE)), FilterUtils.enrichLikeStatements(taskFilter.getFieldFilters().get(taskField))));
-                    case DESCRIPTION -> predicates.add(cb.like(cb.lower(root.get(TaskEntity_.DESCRIPTION)), FilterUtils.enrichLikeStatements(taskFilter.getFieldFilters().get(taskField))));
-                    case STATUS -> predicates.add(cb.equal(root.get(TaskEntity_.STATUS), TaskStatus.valueOf(taskFilter.getFieldFilters().get(taskField))));
-                    case ESTIMATE -> predicates.add(cb.equal(root.get(TaskEntity_.ESTIMATE), taskFilter.getFieldFilters().get(taskField)));
-                    case ACTIVE -> {
-                        predicates.add(taskFilter.getFieldFilters().get(taskField).equals("true") ?
-                        cb.isTrue(root.get(TaskEntity_.ACTIVE)) :
-                        cb.isFalse(root.get(TaskEntity_.ACTIVE)));
-                    }
-                }
-            }
+            var predicatesFromFilter = taskFilter.getFieldFilters().entrySet().stream()
+                    .map(entry -> this.getPredicate(entry.getKey(), entry.getValue(), root, cb))
+                    .toList();
+            predicates.addAll(predicatesFromFilter);
 
             return cb.and(predicates.toArray(Predicate[]::new));
         }, pageable).toList();
+    }
+
+    default Predicate getPredicate(TaskField taskField, String value, Root<TaskEntity> root, CriteriaBuilder cb) {
+        return switch (taskField) {
+            case ID -> cb.equal(root.get(TaskEntity_.ID), value);
+            case NAME_SIMPLE ->
+                    cb.like(cb.lower(root.get(TaskEntity_.NAME_SIMPLE)), FilterUtils.enrichLikeStatements(value));
+            case CUSTOM_ID ->
+                    cb.like(root.get(TaskEntity_.CUSTOM_ID).as(String.class), FilterUtils.enrichLikeStatements(value));
+            case NOTE -> cb.like(cb.lower(root.get(TaskEntity_.NOTE)), FilterUtils.enrichLikeStatements(value));
+            case DESCRIPTION ->
+                    cb.like(cb.lower(root.get(TaskEntity_.DESCRIPTION)), FilterUtils.enrichLikeStatements(value));
+            case STATUS -> cb.equal(root.get(TaskEntity_.STATUS), TaskStatus.valueOf(value));
+            case ESTIMATE -> cb.equal(root.get(TaskEntity_.ESTIMATE), value);
+            case ACTIVE -> value.equalsIgnoreCase("true") ?
+                    cb.isTrue(root.get(TaskEntity_.ACTIVE)) :
+                    cb.isFalse(root.get(TaskEntity_.ACTIVE));
+        };
     }
 }
